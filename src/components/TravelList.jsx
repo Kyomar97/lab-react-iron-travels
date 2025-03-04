fc0c272f7b36e3a9cb908d2c99dc618e6c8fdf20
Sebna Omar <omargr97@gmail.com>
2025-03-04 11:57:36 +0000

    done

---
+++ src/components/TravelList.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import travelPlansData from "../assets/travel-plans.json";
+import TravelPlanCard from "./TravelPlanCard";
+import Favorites from "./Favorites";
+export default function TravelList() {
+  const [travel, setTravel] = useState(travelPlansData);
+  const [favorites, setFavorites] = useState([]);
+  const deleteTravel = (idDelete) => {
+    const deleteTravelList = travel.filter((i) => i.id !== idDelete);
+    setTravel(deleteTravelList);
+  };
+  const addFavorites = (favo) => {
+    setFavorites([...favorites, favo]);
+  };
+  return (
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <div>
+        {travel.map((item) => (
+          <TravelPlanCard
+            key={item.id}
+            itemTravel={item}
+            travelDel={deleteTravel}
+            add={addFavorites}
+          />
+        ))}
+      </div>
+      <div>
+        <h2>Favorites</h2>
+        {favorites.map((item) => (
+          <Favorites key={item.id} itemFavorite={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
